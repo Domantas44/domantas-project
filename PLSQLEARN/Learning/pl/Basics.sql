011860ae9138984df07ee6410a9dbcce8a2e2676
Domantas44 <159730494+Domantas44@users.noreply.github.com>
2025-04-01 20:12:33 +0300

    testingtesting

---
+++ PLSQLEARN/Learning/pl/Basics.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees1 (
+    employee_id NUMBER PRIMARY KEY,
+    first_name VARCHAR2 (50),
+    last_name VARCHAR2 (50),
+    email VARCHAR2 (100),
+    hire_date DATE,
+    salary NUMBER
+);
